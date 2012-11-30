180a82710638a2d804b905edeefa3ac10778d72b
Federico Spini <federico.spini@gmail.com>
2012-11-30 12:52:23 -0800

    crumbs

--- zero/lib/zero.js
+++ zero/lib/zero.js
@@ -2,7 +2,7 @@
 /* !
  * {{project.name}}
  * {{project.description}}
- * Copyright (c) 2012 {{author.name}} <{{author.email}}> ({{author.site}})
+ * Copyright (c) 2012 Enrico Marino e Federico Spini
  * MIT License
  */
 
